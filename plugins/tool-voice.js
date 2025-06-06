ae30fa76270ab8dc03a1b611ceeb37b7ebdc48ac
Dark-Ruzi <pasindusbandara923@gmail.com>
2025-06-06 08:57:35 +0530

    Update tool-voice.js

--- plugins/tool-voice.js
+++ plugins/tool-voice.js
@@ -73,7 +73,7 @@ async (conn, mek, m, {
 
         // Send menu message with image
         const sentMsg = await conn.sendMessage(from, {  
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://files.catbox.moe/aj1z1r.jpg" },
             caption: menuText
         }, { quoted: m });
 
