d33da0f5cc0d60c7a9f2157dc5f4d6e6393caa1c
Dark-Ruzi <pasindusbandara923@gmail.com>
2025-06-09 14:34:58 +0530

    Update emotions-tool.js

--- plugins/emotions-tool.js
+++ plugins/emotions-tool.js
@@ -1,4 +1,43 @@
-const { cmd } = require('../command');🫀
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "happy",
+    desc: "Displays a dynamic edit msg for fun.",
+    category: "tools",
+    react: "😂",
+    filename: __filename
+},
+async (conn, mek, m, { from, reply }) => {
+    try {
+        const loadingMessage = await conn.sendMessage(from, { text: '😂' });
+        const emojiMessages = [
+            "😃", "😄", "😁", "😊", "😎", "🥳",
+            "😸", "😹", "🌞", "🌈", "😃", "😄",
+            "😁", "😊", "😎", "🥳", "😸", "😹",
+            "🌞", "🌈", "😃", "😄", "😁", "😊"
+        ];
+
+        for (const line of emojiMessages) {
+            await new Promise(resolve => setTimeout(resolve, 1000)); // Delay for 1 second
+            await conn.relayMessage(
+                from,
+                {
+                    protocolMessage: {
+                        key: loadingMessage.key,
+                        type: 14,
+                        editedMessage: {
+                            conversation: line,
+                        },
+                    },
+                },
+                {}
+            );
+        }
+    } catch (e) {
+        console.log(e);
+        reply(`❌ *Error!* ${e.message}`);
+    }
+});
 
 cmd({
     pattern: "heart",
